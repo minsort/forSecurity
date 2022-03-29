eae3ddc96eeaa8b87badb579ed58b3a19d08a869
minsort <minsort@yandex.ru>
2022-03-29 22:50:44 +0300

    Added file configuration of security

--- src/main/java/com/project/securityRest/rest/AuthenticationRestControllerV1.java
+++ src/main/java/com/project/securityRest/rest/AuthenticationRestControllerV1.java
@@ -33,23 +33,19 @@ public class AuthenticationRestControllerV1 {
         this.userService = userService;
     }
     public ResponseEntity login (@RequestBody AuthenticationRequestDto requestDto){
-        try{
-            String username = requestDto.getUsername();
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username,
-                    requestDto.getPassword()));
-            User user = userService.findByUsername(username);
+        String username = requestDto.getUsername();
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username,
+                requestDto.getPassword()));
+        User user = userService.findByUsername(username);
 
-            if(user==null){
-                throw new UsernameNotFoundException("User with username: "+username+" not found");
-            }
-            String token = jwtTokenProvider.createToken(username, user.getRoles());
-
-            Map<Object, Object> response = new HashMap<>();
-            response.put("username", username);
-            response.put("token", token);
-return ResponseEntity.ok(response);
-        }catch (AuthenticationException e){
-            throw new BadCredentialsException("Invalid username or password");
+        if(user==null){
+            throw new UsernameNotFoundException("User with username: "+username+" not found");
         }
+        String token = jwtTokenProvider.createToken(username, user.getRoles());
+
+        Map<Object, Object> response = new HashMap<>();
+        response.put("username", username);
+        response.put("token", token);
+        return ResponseEntity.ok(response);
     }
 }
