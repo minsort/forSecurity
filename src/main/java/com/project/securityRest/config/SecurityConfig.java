9ec540d7e3e6a1e63f22239f39618691a3887d82
minsort <minsort@yandex.ru>
2022-03-31 13:57:23 +0300

    500 ошибка!

--- src/main/java/com/project/securityRest/config/SecurityConfig.java
+++ src/main/java/com/project/securityRest/config/SecurityConfig.java
@@ -7,10 +7,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final JwtTokenProvider jwtTokenProvider;
 
